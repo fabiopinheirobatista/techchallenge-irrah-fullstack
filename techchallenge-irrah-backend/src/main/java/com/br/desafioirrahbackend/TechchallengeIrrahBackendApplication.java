97ee0ec4a9560262720752a0a42dccbc9261cbca
fabiobatistabertioga <fabiobatista.bertioga@gmail.com>
2026-06-27 18:59:09 -0300

    Added .gitignore file

---
+++ techchallenge-irrah-backend/src/main/java/com/br/desafioirrahbackend/TechchallengeIrrahBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.desafioirrahbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeIrrahBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechchallengeIrrahBackendApplication.class, args);
+    }
+
+}
---
+++ techchallenge-irrah-backend/src/test/java/com/br/desafioirrahbackend/TechchallengeIrrahBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.desafioirrahbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengeIrrahBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
